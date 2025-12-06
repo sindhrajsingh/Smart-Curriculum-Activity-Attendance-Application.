babaa32ccae99cc7c806eac804fb270d5db9b29a
SINDHRAJ SINGH <srthakur7824@gmail.com>
2025-12-06 17:59:25 +0530

    Create Reports.js

---
+++ Frontend/src/pages/Reports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1>Reports</h1>
+      <p>Reports and analytics will be shown here.</p>
+    </div>
+  );
+};
+
+export default Reports;
