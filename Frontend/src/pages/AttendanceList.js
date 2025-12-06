11cf359f178a6e31a1a6172e7ddfe30917a79e55
SINDHRAJ SINGH <srthakur7824@gmail.com>
2025-12-06 17:53:30 +0530

    Add AttendanceList component with placeholder UI

---
+++ Frontend/src/pages/AttendanceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AttendanceList = () => {
+  return (
+    <div>
+      <h1>Attendance</h1>
+      <p>Attendance management UI coming soon.</p>
+    </div>
+  );
+};
+
+export default AttendanceList;
