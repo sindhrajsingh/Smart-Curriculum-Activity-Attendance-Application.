6e53be88a4ad81191ffcc38b1da7b577735484f2
SINDHRAJ SINGH <srthakur7824@gmail.com>
2025-12-06 17:54:53 +0530

    Create StudentList.js

---
+++ Frontend/src/pages/StudentList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentList = () => {
+  return (
+    <div>
+      <h1>Students</h1>
+      <p>Student management UI coming soon.</p>
+    </div>
+  );
+};
+
+export default StudentList;
