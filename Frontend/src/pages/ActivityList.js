73e8c34cdf56164dc10d4f6916862a5388aa0f38
SINDHRAJ SINGH <srthakur7824@gmail.com>
2025-12-06 17:53:57 +0530

    Create ActivityList.js

---
+++ Frontend/src/pages/ActivityList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ActivityList = () => {
+  return (
+    <div>
+      <h1>Activities</h1>
+      <p>Activity management UI coming soon.</p>
+    </div>
+  );
+};
+
+export default ActivityList;
